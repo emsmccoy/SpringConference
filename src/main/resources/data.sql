4dd19869cfe5b56800d370965e7b2faa91581358
emsmccoy <65463392+emsmccoy@users.noreply.github.com>
2024-11-17 20:18:25 +0100

    Update data.sql. Error in loading conferences

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,14 @@ INSERT INTO books (id, title, author, isbn) VALUES
 (RANDOM_UUID(), 'Spring Security in Action', 'Laurentiu Spilca', '9781617297731'),
 (RANDOM_UUID(), 'Reactive Spring', 'Josh Long', '9781732910225'),
 (RANDOM_UUID(), 'Native Image Definitive Guide', 'Oleg Šelajev', '9781492078531');
+
+INSERT INTO speakers (id, name) VALUES
+(RANDOM_UUID(), 'Brandon Sanderson'),
+(RANDOM_UUID(), 'Uncle Bob'),
+(RANDOM_UUID(), 'Ada Lovelace');
+
+--Error: Failed to execute SQL script statement #3 of class path resource [data.sql]: INSERT INTO conferences (id, name, conferenceDate) VALUES (RANDOM_UUID(), 'Spring Boot 101', '2025-03-20')
+-- possibly LocalDate issue
+--INSERT INTO conferences (id, name, date) VALUES
+--(RANDOM_UUID(), 'Spring Boot 101', '2025-03-20');
+--(RANDOM_UUID(), 'Spring Boot Is Awesome', '2025-03-22');
